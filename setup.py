11f75e20a610df55001c252a41ee3a9d88ab3c40
udoniyor <doniyor@live.com>
2015-08-23 01:04:19 +0100

    Added setup and requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+Setup script for BrojectB.
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='projectb',
+      version='0.0.1',
+      author='Doniyor Ulmasov',
+      author_email='du514@ic.ac.uk',
+      description='A Python package for GUI Bayesian optimization',
+      url='http://github.com/udoniyor/projectb',
+      license='Simplified BSD',
+      packages=find_packages(),
+      package_data={'': ['*.txt', '*.npz']},
+      install_requires=['numpy', 'scipy', 'matplotlib', 'mwhutils', 'pygp', 'pybo'])
